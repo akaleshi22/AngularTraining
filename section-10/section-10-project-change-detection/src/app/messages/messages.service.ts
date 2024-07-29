36e38964b4d65d35866c1033a43aa53192b77869
akaleshi22 <akaleshi@kaartech.com>
2024-07-29 10:21:07 -0500

    Section 10 Done

---
+++ section-10/section-10-project-change-detection/src/app/messages/messages.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, signal } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MessagesService {
+  private messages = signal<string[]>([]);
+
+  allMessages = this.messages.asReadonly();
+
+  addMessage(message: string) {
+    this.messages.update((oldMessages) => {
+      return [...oldMessages, message]
+    })
+  }
+}
