cdd7e764272585ce1cd30efda566ff12f73008cb
akaleshi22 <akaleshi@kaartech.com>
2024-07-30 13:16:22 -0500

    Section 15 Routing Done

--- section-14/section-14-project-routing/src/app/users/user/user.component.ts
+++ section-14/section-14-project-routing/src/app/users/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, computed, input } from '@angular/core';
 
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { type User } from './user.model';
 
 @Component({
@@ -7,6 +8,7 @@ import { type User } from './user.model';
   standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
+  imports: [RouterLink, RouterLinkActive],
 })
 export class UserComponent {
   user = input.required<User>();
